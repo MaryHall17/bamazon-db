3eabcf18b97390c5abb202753cdefca63c49bd05
Mary Hall <mh6uj@students-MBP.home>
2018-01-30 15:36:40 -0500

    positive if/else logic without update

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -28,7 +28,39 @@ function queryStore () {
 		for (var i = 0; i < res.length; i++) {
 			console.log("Item ID: " + res[i].item_id + " | " + "Product: " + res[i].product_name + " | " +  "Department: " + res[i].department_name + " | " + "Price: " + res[i].price);
 		}
+	customerBuysProduct();
 	});
 }
 
 var inquirer = require("inquirer");
+
+function customerBuysProduct () {
+	inquirer.prompt([
+	{
+		type: "input",
+		message: "Welcome to Bamazon! Please select an item to purchase via item id.",
+		name: "item_id"
+		
+	},
+	{
+		type: "input",
+		message: "How much would you like to buy? Please enter a numeric value.",
+		name: "requested_quantity"
+	}
+]).then(function(inquirerResponse) {
+	// console.log(inquirerResponse.requested_quantity);
+	// Query the store for inputted item only
+	var query = connection.query("SELECT item_id, product_name, department_name, price, stock_quantity FROM products WHERE item_id = ?", [inquirerResponse.item_id], function(err, res) {
+		for (var i = 0; i < res.length; i++) {
+			// console.log(res[i].price);
+			//If/else logic based on quantity:
+			if (inquirerResponse.requested_quantity <= res[i].stock_quantity) {
+				console.log("Your total for " + res[i].product_name + " at quantity: " + inquirerResponse.requested_quantity + " is " + res[i].price * inquirerResponse.requested_quantity + " \nThrough our super shady predictive analytics, we have already billed you at your preferred payment method. Thanks for choosing Bamazon!");
+			}
+		}
+	})
+
+});
+
+}
+
